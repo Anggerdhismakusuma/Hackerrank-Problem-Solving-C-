3ae8ef221b916016cc07891d0a3a5cae0a1df224
Anggerdhismakusuma <kangger97@gmail.com>
2025-03-09 20:51:22 +0700

    Add Simple Array Sum Solution

---
+++ Simple Array Sum.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int n;
+    scanf("%d", &n);
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+        int num;
+        scanf("%d", &num);
+        sum += num;
+    }
+    printf("%d", sum);
+    return 0;
+}
